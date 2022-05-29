116a85001bc835802a62681e06f2585563ba7554
akshaykaura <akshaykaura0001@gmail.com>
2022-05-29 14:14:59 +0530

    restricted back action after logout to not show logged in data again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   add_flash_types :info, :success, :warning
 
   # before any action runs, it will first run this method
-  before_action :set_current_user
+  before_action :set_current_user, :set_cache_headers
 
+  private
   # if there is a session id i.e. user logged in then find it in db and assign it to instance var
   def set_current_user
     if session[:user_id]
@@ -15,4 +16,11 @@ class ApplicationController < ActionController::Base
   end
   # if this fails then it will throw an error that it can't find that id in db if we used .find()
   # so we use .find_by() so that it doesn't crash the app
+
+  # restrict going back to logged in app pages after logout by not storing the cache
+  def set_cache_headers
+    response.headers["Cache-Control"] = "no-cache, no-store"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Mon, 01 Jan 1990 00:00:00 GMT"
+  end
 end
