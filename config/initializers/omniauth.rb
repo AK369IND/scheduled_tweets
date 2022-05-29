8e73d7006098fca5ae0ef144369b57d5a32139ce
akshaykaura <akshaykaura0001@gmail.com>
2022-05-29 14:18:18 +0530

    each oauth twitter request is now preceeded with a login to twitter modal so as to have multiple twitter accounts linked to a single user

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.credentials.dig(:twitter, :api_key), Rails.application.credentials.dig(:twitter, :api_secret)
+  provider :twitter, Rails.application.credentials.dig(:twitter, :api_key), Rails.application.credentials.dig(:twitter, :api_secret),
+    {
+      secure_image_url: true,
+      authorize_params: {
+        force_login: true,
+        lang: 'en'
+      }
+    }
 end
